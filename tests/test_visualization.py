21e00f0fb5c3f90424bace9fc2e65093aa3d3497
anushka255 <aa9078@princeton.edu>
2024-08-19 17:08:08 -0400

    added test case for generalized procrustes analysis

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -2,7 +2,11 @@ import numpy as np
 import pandas as pd
 import scanpy as sc
 from pathlib import Path
-from paste.visualization import stack_slices_pairwise, stack_slices_center, plot_slice
+from paste.visualization import (
+    stack_slices_pairwise,
+    stack_slices_center,
+    generalized_procrustes_analysis,
+)
 from pandas.testing import assert_frame_equal
 
 test_dir = Path(__file__).parent
@@ -59,7 +63,7 @@ def test_stack_slices_center(slices):
     )
     assert_frame_equal(
         pd.DataFrame(new_center.obsm["spatial"], columns=["0", "1"]),
-        pd.read_csv(output_dir / f"aligned_spatial_center.csv"),
+        pd.read_csv(output_dir / "aligned_spatial_center.csv"),
     )
 
     for i, slice in enumerate(new_slices):
@@ -87,5 +91,56 @@ def test_stack_slices_center(slices):
     assert np.all(
         np.isclose(
             expected_translations, translations, rtol=1e-05, atol=1e-08, equal_nan=True
+        )
+    )
+
+
+def test_generalized_procrustes_analysis(slices):
+    center_slice = sc.read_h5ad(input_dir / "center_slice.h5ad")
+
+    pairwise_info = np.genfromtxt(
+        input_dir / "center_slice1_pairwise.csv", delimiter=","
+    )
+
+    aligned_center, aligned_slice, theta, translation_x, translation_y = (
+        generalized_procrustes_analysis(
+            center_slice.obsm["spatial"],
+            slices[0].obsm["spatial"],
+            pairwise_info,
+            output_params=True,
+        )
+    )
+
+    assert_frame_equal(
+        pd.DataFrame(aligned_center, columns=['0', '1']),
+        pd.read_csv(output_dir / "aligned_center.csv"),
+    )
+    assert_frame_equal(
+        pd.DataFrame(aligned_slice, columns=['0', '1']),
+        pd.read_csv(output_dir / "aligned_slice.csv"),
+    )
+    expected_theta = 0.0
+    expected_translation_x = [16.44623228, 16.73757874]
+    expected_translation_y = [16.44623228, 16.73757874]
+
+    assert np.all(
+        np.isclose(expected_theta, theta, rtol=1e-05, atol=1e-08, equal_nan=True)
+    )
+    assert np.all(
+        np.isclose(
+            expected_translation_x,
+            translation_x,
+            rtol=1e-05,
+            atol=1e-08,
+            equal_nan=True,
+        )
+    )
+    assert np.all(
+        np.isclose(
+            expected_translation_y,
+            translation_y,
+            rtol=1e-05,
+            atol=1e-08,
+            equal_nan=True,
         )
     )

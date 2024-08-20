b756ddd1a2c156acb565663d11dfbe66715f9538
anushka255 <aa9078@princeton.edu>
2024-08-20 16:45:22 -0400

    using mark.skip instead of -k flag on cli

--- tests/test_paste2.py
+++ tests/test_paste2.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pandas as pd
 from paste2.PASTE2 import partial_pairwise_align
+import pytest
 
 test_dir = Path(__file__).parent
 input_dir = test_dir / "data/input"
@@ -10,6 +11,7 @@ from pandas.testing import assert_frame_equal
 
 
 # TODO: this takes 3 years to pass, see whats going on here
+@pytest.mark.skip
 def test_partial_pairwise_align(slices2):
     pi_BC = partial_pairwise_align(slices2[0], slices2[1], s=0.7)
 

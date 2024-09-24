64383fb6d8f01e67e4705159552b17232e1f4963
anushka255 <aa9078@princeton.edu>
2024-09-24 13:37:44 -0400

    some more parameterization

--- tests/test_paste2.py
+++ tests/test_paste2.py
@@ -19,12 +19,38 @@ input_dir = test_dir / "data/input"
 output_dir = test_dir / "data/output"
 
 
-def test_partial_pairwise_align(slices2):
-    pi_BC = partial_pairwise_align(slices2[0], slices2[1], s=0.7)
+def pytest_generate_tests(metafunc):
+    if "loss_fun" in metafunc.fixturenames:
+        metafunc.parametrize(
+            "loss_fun, filename",
+            [
+                ("square_loss", "gwloss_partial.csv"),
+                ("kl_loss", "gwloss_partial_kl_loss.csv"),
+            ],
+        )
+    if "dissimilarity" in metafunc.fixturenames:
+        metafunc.parametrize(
+            "dissimilarity, filename",
+            [
+                ("euc", "partial_pairwise_align_euc.csv"),
+                ("gkl", "partial_pairwise_align_gkl.csv"),
+                ("kl", "partial_pairwise_align_kl.csv"),
+                ("selection_kl", "partial_pairwise_align_selection_kl.csv"),
+                ("pca", "partial_pairwise_align_pca.csv"),
+                ("glmpca", "partial_pairwise_align_glmpca.csv"),
+            ],
+        )
+
+
+def test_partial_pairwise_align(slices2, dissimilarity, filename):
+    pi_BC = partial_pairwise_align(
+        slices2[0], slices2[1], s=0.7, dissimilarity=dissimilarity
+    )
+    pd.DataFrame(pi_BC).to_csv(output_dir / filename, index=False)
 
     assert_frame_equal(
         pd.DataFrame(pi_BC, columns=[str(i) for i in range(pi_BC.shape[1])]),
-        pd.read_csv(output_dir / "partial_pairwise_align.csv"),
+        pd.read_csv(output_dir / filename),
         rtol=1e-03,
         atol=1e-03,
     )
@@ -65,10 +91,11 @@ def test_partial_pairwise_align_histology(slices2):
     pairwise_info, log = partial_pairwise_align_histology(
         slices2[0], slices2[1], s=0.7, return_obj=True, dissimilarity="euclidean"
     )
-    assert log == 78.30015827691841
+    assert round(log, 3) == round(78.30015827691841, 3)
     assert_frame_equal(
         pd.DataFrame(pairwise_info, columns=[str(i) for i in range(2877)]),
         pd.read_csv(output_dir / "partial_pairwise_align_histology.csv"),
+        rtol=1e-05,
     )
 
 
@@ -162,7 +189,7 @@ def test_gloss_partial(slices):
     assert output == expected_output
 
 
-def test_gwloss_partial(slices):
+def test_gwloss_partial(slices, loss_fun, filename):
     common_genes = intersect(slices[1].var.index, slices[2].var.index)
     sliceA = slices[1][:, common_genes]
     sliceB = slices[2][:, common_genes]
@@ -187,9 +214,9 @@ def test_gwloss_partial(slices):
         np.ones((sliceB.shape[0],)) / sliceB.shape[0],
     )
 
-    output = gwgrad_partial(distance_a, distance_b, G0, loss_fun="square_loss")
+    output = gwgrad_partial(distance_a, distance_b, G0, loss_fun=loss_fun)
 
     assert_frame_equal(
         pd.DataFrame(output, columns=[str(i) for i in range(264)]),
-        pd.read_csv(output_dir / "gwloss_partial.csv"),
+        pd.read_csv(output_dir / filename),
     )

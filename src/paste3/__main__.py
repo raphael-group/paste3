02ab1fe7b726c0171d7543b6c7a4fabfb0afd2b5
anushka255 <aa9078@princeton.edu>
2024-10-21 14:43:38 -0400

    added __main__

---
+++ src/paste3/__main__.py
@@ -0,0 +1,41 @@
+import logging
+import argparse
+import os
+from paste3 import align
+import paste3
+
+logger = logging.getLogger("paste3")
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--version", action="version", version=paste3.__version__)
+
+    modules = [align]
+
+    subparsers = parser.add_subparsers(title="Choose a command")
+    subparsers.required = True
+
+    def get_str_name(module):
+        return os.path.splitext(os.path.basename(module.__file__))[0]
+
+    for module in modules:
+        this_parser = subparsers.add_parser(
+            get_str_name(module), description=module.__doc__
+        )
+        this_parser.add_argument(
+            "-v", "--verbose", action="store_true", help="Increase verbosity"
+        )
+
+        module.add_args(this_parser)
+        this_parser.set_defaults(func=module.main)
+
+    args = parser.parse_args()
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()

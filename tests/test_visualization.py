ab6f07e86f1eb2fa8cc7ad95774451725fcc9c04
anushka255 <aa9078@princeton.edu>
2024-11-24 21:28:16 -0500

    regenerated test file for test_visualization

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 
 import numpy as np
-import pandas as pd
 import scanpy as sc
 import torch
-from pandas.testing import assert_frame_equal
 
 from paste3.visualization import (
     generalized_procrustes_analysis,
@@ -12,66 +10,48 @@ from paste3.visualization import (
     stack_slices_pairwise,
 )
 
-test_dir = Path(__file__).parent
-input_dir = test_dir / "data/input"
-output_dir = test_dir / "data/output"
+test_dir = Path(__file__).parent / "data"
 
 
 def test_stack_slices_pairwise(slices):
     n_slices = len(slices)
 
+    data = np.load(test_dir / "stack_slices_pairwise.npz")
     pairwise_info = [
-        torch.Tensor(
-            np.genfromtxt(input_dir / f"slices_{i}_{i + 1}_pairwise.csv", delimiter=",")
-        ).double()
-        for i in range(1, n_slices)
+        torch.Tensor(data[f"pi_{i}_{i+1}"]).double() for i in range(n_slices - 1)
     ]
 
     new_slices, thetas, translations = stack_slices_pairwise(slices, pairwise_info)
 
-    for i, slice in enumerate(new_slices, start=1):
-        assert_frame_equal(
-            pd.DataFrame(slice.obsm["spatial"], columns=["0", "1"]),
-            pd.read_csv(output_dir / f"aligned_spatial_{i}_{i + 1}.csv"),
-            atol=1e-6,
-        )
+    for i, slice in enumerate(new_slices):
+        assert np.allclose(slice.obsm["spatial"], data[f"aligned_{i}"])
 
-    expected_thetas = [-0.25086326614894794, 0.5228805289947901, 0.02478065908672744]
+    expected_thetas = [-0.25086326663252634, 0.5228805221288383, 0.024780658882267766]
     expected_translations = [
         ([16.44623233, 16.73757875], [19.80709569, 15.74706369]),
         ([-2.90017423e-08, -1.19685091e-08], [16.32537929, 17.43314825]),
         ([1.58526981e-07, 6.97949045e-07], [19.49901545, 17.35546584]),
     ]
 
-    assert np.allclose(expected_thetas, thetas, rtol=1e-05, atol=1e-08)
-    assert np.allclose(expected_translations, translations, rtol=1e-05, atol=1e-08)
+    assert np.allclose(expected_thetas, thetas)
+    assert np.allclose(expected_translations, translations)
 
 
 def test_stack_slices_center(slices):
-    center_slice = sc.read_h5ad(input_dir / "center_slice.h5ad")
-
+    center_slice = sc.read_h5ad(test_dir / "center_slice.h5ad")
+    data = np.load(test_dir / "stack_slices_center.npz")
     pairwise_info = [
-        torch.Tensor(
-            np.genfromtxt(input_dir / f"center_slice{i}_pairwise.csv", delimiter=",")
-        ).double()
-        for i in range(1, len(slices) + 1)
+        torch.Tensor(data[f"pi_{i}_{i+1}"]).double() for i in range(len(slices))
     ]
 
     new_center, new_slices, thetas, translations = stack_slices_center(
         center_slice, slices, pairwise_info
     )
-    assert_frame_equal(
-        pd.DataFrame(new_center.obsm["spatial"], columns=["0", "1"]),
-        pd.read_csv(output_dir / "aligned_spatial_center.csv"),
-        atol=1e-6,
-    )
+
+    assert np.allclose(new_center.obsm["spatial"], data["aligned"])
 
     for i, slice in enumerate(new_slices):
-        assert_frame_equal(
-            pd.DataFrame(slice.obsm["spatial"], columns=["0", "1"]),
-            pd.read_csv(output_dir / f"slice{i}_stack_slices_center.csv"),
-            atol=1e-6,
-        )
+        assert np.allclose(data[f"new_slice_{i}"], slice.obsm["spatial"])
 
     expected_thetas = [
         0.0,
@@ -86,84 +66,49 @@ def test_stack_slices_center(slices):
         ([16.44623234, 16.73757877], [19.49901525, 17.35546567]),
     ]
 
-    assert np.allclose(expected_thetas, thetas, rtol=1e-05, atol=1e-08)
-    assert np.allclose(expected_translations, translations, rtol=1e-05, atol=1e-08)
+    assert np.allclose(expected_thetas, thetas)
+    assert np.allclose(expected_translations, translations)
 
 
 def test_generalized_procrustes_analysis(slices):
-    center_slice = sc.read_h5ad(input_dir / "center_slice.h5ad")
-
-    pairwise_info = torch.Tensor(
-        np.genfromtxt(input_dir / "center_slice1_pairwise.csv", delimiter=",")
-    ).double()
+    center_slice = sc.read_h5ad(test_dir / "center_slice.h5ad")
+    data = np.load(test_dir / "generalized_procrustes_analysis.npz")
 
     aligned_center, aligned_slice, theta, translation_x, translation_y = (
         generalized_procrustes_analysis(
             torch.Tensor(center_slice.obsm["spatial"]).double(),
             torch.Tensor(slices[0].obsm["spatial"]).double(),
-            pairwise_info,
+            torch.Tensor(data["pi"]).double(),
         )
     )
+    assert np.allclose(aligned_center, data["aligned_center"])
+    assert np.allclose(aligned_slice, data["aligned_slice"])
 
-    assert_frame_equal(
-        pd.DataFrame(aligned_center, columns=["0", "1"]),
-        pd.read_csv(output_dir / "aligned_center.csv"),
-        atol=1e-6,
-    )
-    assert_frame_equal(
-        pd.DataFrame(aligned_slice, columns=["0", "1"]),
-        pd.read_csv(output_dir / "aligned_slice.csv"),
-        atol=1e-6,
-    )
     expected_theta = 0.0
     expected_translation_x = [16.44623228, 16.73757874]
     expected_translation_y = [16.44623228, 16.73757874]
 
-    assert np.all(
-        np.isclose(expected_theta, theta, rtol=1e-05, atol=1e-08, equal_nan=True)
-    )
-    assert np.all(
-        np.isclose(
-            expected_translation_x,
-            translation_x,
-            rtol=1e-05,
-            atol=1e-08,
-            equal_nan=True,
-        )
-    )
-    assert np.all(
-        np.isclose(
-            expected_translation_y,
-            translation_y,
-            rtol=1e-05,
-            atol=1e-08,
-            equal_nan=True,
-        )
-    )
+    assert np.allclose(expected_theta, theta)
+    assert np.allclose(expected_translation_x, translation_x)
+    assert np.allclose(expected_translation_y, translation_y)
 
 
 def test_partial_stack_slices_pairwise(slices):
     n_slices = len(slices)
+    data = np.load(test_dir / "partial_stack_slices_pairwise.npz")
 
     pairwise_info = [
-        torch.Tensor(
-            np.genfromtxt(input_dir / f"slices_{i}_{i + 1}_pairwise.csv", delimiter=",")
-        ).double()
-        for i in range(1, n_slices)
+        torch.Tensor(data[f"pi_{i}_{i+1}"]).double() for i in range(n_slices - 1)
     ]
 
     new_slices, _, _ = stack_slices_pairwise(slices, pairwise_info, is_partial=True)
 
-    for i, slice in enumerate(new_slices, start=1):
-        assert_frame_equal(
-            pd.DataFrame(slice.obsm["spatial"], columns=["0", "1"]),
-            pd.read_csv(output_dir / f"aligned_spatial_{i}_{i + 1}.csv"),
-            atol=1e-6,
-        )
+    for i, slice in enumerate(new_slices):
+        assert np.allclose(slice.obsm["spatial"], data[f"aligned_{i}"])
 
 
 def test_partial_procrustes_analysis(slices2):
-    data = np.load(output_dir / "partial_procrustes_analysis.npz")
+    data = np.load(test_dir / "partial_procrustes_analysis.npz")
 
     assert torch.sum(torch.Tensor(data["pi"])) < 0.99999999
 
